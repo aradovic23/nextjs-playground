cd7d147f86034476e3248d7c9ab9a9f7f3eeb7f0
Aleksa Radovic <aleksa.radovic@rechargeapps.com>
2023-08-03 01:05:01 +0200

    h1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 export default function Home() {
 
   return (
-    <main className="grid h-screen grid-cols-2 gap-5 place-items-center">
-      <Link className="text-blue-500 underline" href="/tables/styled">styled table</Link>
-      <Link className="text-blue-500 underline" href="/tables/unstyled">unstyled table</Link>
+    <main className="grid h-screen place-items-center">
+      <h1 className="text-2xl font-bold">Playground </h1>
+      <div className="grid grid-cols-2 gap-5 place-items-center">
+        <Link className="text-blue-500 underline" href="/tables/styled">styled table</Link>
+        <Link className="text-blue-500 underline" href="/tables/unstyled">unstyled table</Link>
+      </div>
     </main>
   )
 }
